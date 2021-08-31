86cead728a9a56d8236f1c4af261ae0e85b56bc1
nhat anh <anhnhattran97@gmail.com>
2021-08-31 22:29:28 +0700

    Add Film and switch code from Dashboard Component to Films Component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import AuthRoute from './HOC/AuthRoute';
-import MainLayout from './HOC/MainLayout';
-import PrivateRoute from './HOC/PrivateRoute';
-import Home from './View/Home';
-import PageNotFound from './View/PageNotFound';
-import SignIn from './View/SignIn';
-import Profile from './View/Profile';
-import './App.css';
-import AdminLayout from './HOC/AdminLayout';
-import Dashboard from './View/Admin/Dashboard';
-import Films from './View/Admin/Films';
-import Showtime from './View/Admin/Showtime';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import AuthRoute from "./HOC/AuthRoute";
+import MainLayout from "./HOC/MainLayout";
+import PrivateRoute from "./HOC/PrivateRoute";
+import Home from "./View/Home";
+import PageNotFound from "./View/PageNotFound";
+import SignIn from "./View/SignIn";
+import Profile from "./View/Profile";
+import "./App.css";
+import AdminLayout from "./HOC/AdminLayout";
+import Dashboard from "./View/Admin/Dashboard";
+import Films from "./View/Admin/Films";
+import NewFilm from "./View/Admin/NewFilm";
+import Showtime from "./View/Admin/Showtime";
+import EditFilm from "./View/Admin/EditFilm";
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact>
-          <Redirect to='/home' />
+        <Route path="/" exact>
+          <Redirect to="/home" />
         </Route>
-        <Route path='/admin/:path?' exact>
+        <Route path="/admin/:path?/:path?/:path?" exact>
           <AdminLayout>
-            <Route path='/admin/home' exact component={Dashboard} />
-            <Route path='/admin/films' exact component={Films} />
-            <Route path='/admin/showtime' exact component={Showtime} />
+            <Route path="/admin" exact component={Dashboard} />
+            <Route path="/admin/films" exact component={Films} />
+            <Route path="/admin/films/addnew" exact component={NewFilm} />
+            <Route path="/admin/films/edit/:id" exact component={EditFilm} />
+            <Route path="/admin/showtime" exact component={Showtime} />
           </AdminLayout>
         </Route>
         <Route>
           <MainLayout>
-            <AuthRoute path='/signin' exact component={SignIn} />
-            <PrivateRoute path='/profile' exact component={Profile} />
-            <Route path='/home' exact component={Home} />
+            <AuthRoute path="/signin" exact component={SignIn} />
+            <PrivateRoute path="/profile" exact component={Profile} />
+            <Route path="/home" exact component={Home} />
           </MainLayout>
         </Route>
         <Route component={PageNotFound} />
--- src/HOC/AdminLayout/index.jsx
+++ src/HOC/AdminLayout/index.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
-import { Layout, Menu } from 'antd';
-import {
-  DesktopOutlined,
-  FileOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import { DesktopOutlined, FileOutlined, UserOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 const { Header, Content, Sider } = Layout;
-// const { SubMenu } = Menu;
+
+const { SubMenu } = Menu;
 
 export default function AdminLayout(props) {
   const [collapsed, setCollapsed] = useState(false);
@@ -16,35 +14,29 @@ export default function AdminLayout(props) {
   };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: "100vh" }}>
       <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-        <div className='logo' />
-        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-          <Menu.Item key='1' icon={<UserOutlined />}>
-           User
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            User
           </Menu.Item>
-          <Menu.Item key='2' icon={<FileOutlined />}>
-            Films
-          </Menu.Item>
-          {/* <SubMenu key='sub1' icon={<UserOutlined />} title='User'>
-            <Menu.Item key='3'>Tom</Menu.Item>
-            <Menu.Item key='4'>Bill</Menu.Item>
-            <Menu.Item key='5'>Alex</Menu.Item>
+          <SubMenu key="sub1" icon={<FileOutlined />} title="Films">
+            <Menu.Item key="2">
+              <NavLink to="/admin/films">Films</NavLink>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <NavLink to="/admin/films/addnew">Add new</NavLink>
+            </Menu.Item>
           </SubMenu>
-          <SubMenu key='sub2'  title='Team'>
-            <Menu.Item key='6'>Team 1</Menu.Item>
-            <Menu.Item key='8'>Team 2</Menu.Item>
-          </SubMenu> */}
-          <Menu.Item key='9' icon={<DesktopOutlined />}>
+          <Menu.Item key="4" icon={<DesktopOutlined />}>
             Showtime
           </Menu.Item>
         </Menu>
       </Sider>
-      <Layout className='site-layout'>
-        <Header className='site-layout-background' style={{ padding: 0 }} />
-        <Content style={{ margin: '10px 16px' }}>
-          {props.children}
-        </Content>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "10px 16px" }}>{props.children}</Content>
       </Layout>
     </Layout>
   );

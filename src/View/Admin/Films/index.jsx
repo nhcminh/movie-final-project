04d8820746c2e0d3d25ef453fcede7c2b9fe9e4b
nhat anh <anhnhattran97@gmail.com>
2021-09-06 19:10:21 +0700

    Search Film

--- src/View/Admin/Films/index.jsx
+++ src/View/Admin/Films/index.jsx
@@ -71,8 +71,6 @@ export default function Dashboard(props) {
 
   // Search
   const handleSearch = (value) => {
-    // console.log(value);
-    // setSearchData(value);
   };
 
   const handleChangeSearch = (e) => {
